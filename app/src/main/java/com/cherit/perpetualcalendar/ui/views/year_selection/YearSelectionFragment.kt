2291c5c7217d2e6bec47568b1ba536103ee0f317
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2021-04-14 16:07:18 +0200

    Year selection bugfix

--- app/src/main/java/com/cherit/perpetualcalendar/ui/views/year_selection/YearSelectionFragment.kt
+++ app/src/main/java/com/cherit/perpetualcalendar/ui/views/year_selection/YearSelectionFragment.kt
@@ -10,9 +10,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.cherit.perpetualcalendar.R
-import com.cherit.perpetualcalendar.models.Event
 import com.cherit.perpetualcalendar.ui.components.event_list.EventListRecyclerViewAdapter
-import java.util.*
+import com.cherit.perpetualcalendar.utils.getMovingEvents
 
 class YearSelectionFragment : Fragment() {
 
@@ -26,29 +25,29 @@ class YearSelectionFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         yearSelectionViewModel =
-                ViewModelProvider(this).get(YearSelectionViewModel::class.java)
+            ViewModelProvider(requireActivity()).get(YearSelectionViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_year_selection, container, false)
         eventsListView = root.findViewById(R.id.events_list)
-        setupNumberPicker(root)
+        initNumberPicker(root)
         setViewModelObservers()
         return root
     }
 
-    private fun setupNumberPicker(root: View) {
+    private fun initNumberPicker(root: View) {
         val numberPicker: NumberPicker = root.findViewById(R.id.year_picker)
         numberPicker.minValue = 1900
         numberPicker.maxValue = 2200
         numberPicker.wrapSelectorWheel = true
-        numberPicker.value = yearSelectionViewModel.selected_year.value!!
+        numberPicker.value = yearSelectionViewModel.selectedYear.value!!
         numberPicker.setOnValueChangedListener { _, _, newVal ->
-            yearSelectionViewModel.selected_year.setValue(newVal)
+            yearSelectionViewModel.selectedYear.value = newVal
         }
     }
 
     private fun setViewModelObservers() {
-        yearSelectionViewModel.selected_year.observe(viewLifecycleOwner, Observer {
-            eventsListView.adapter = EventListRecyclerViewAdapter(yearSelectionViewModel.movingEvents.value!!)
+        yearSelectionViewModel.selectedYear.observe(viewLifecycleOwner, {
+            eventsListView.adapter = EventListRecyclerViewAdapter(getMovingEvents(it))
         })
     }
 }
--- app/src/main/java/com/cherit/perpetualcalendar/ui/views/year_selection/YearSelectionViewModel.kt
+++ app/src/main/java/com/cherit/perpetualcalendar/ui/views/year_selection/YearSelectionViewModel.kt
@@ -4,16 +4,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cherit.perpetualcalendar.models.Event
 import com.cherit.perpetualcalendar.utils.getMovingEvents
+import java.time.LocalDate
 import java.util.*
 import kotlin.collections.ArrayList
 
 class YearSelectionViewModel : ViewModel() {
 
-    private val _selected_year = MutableLiveData<Int>().apply {
-        value = Calendar.getInstance().get(Calendar.YEAR)
-    }
+    var selectedYear: MutableLiveData<Int> = MutableLiveData<Int>(LocalDate.now().year)
 
-    var selected_year: MutableLiveData<Int> = _selected_year
+    var movingEvents: MutableLiveData<ArrayList<Event>> = MutableLiveData<ArrayList<Event>>()
 
-    var movingEvents: MutableLiveData<ArrayList<Event>> = MutableLiveData(getMovingEvents(selected_year.value!!))
 }

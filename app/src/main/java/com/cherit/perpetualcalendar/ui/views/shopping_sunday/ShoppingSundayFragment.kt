bd21bc5961a63f15276b106398fbb3c0d24d68f6
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2021-04-14 16:08:09 +0200

    Shopping saturday - UI and connection to Year Selected ViewModel

--- app/src/main/java/com/cherit/perpetualcalendar/ui/views/shopping_sunday/ShoppingSundayFragment.kt
+++ app/src/main/java/com/cherit/perpetualcalendar/ui/views/shopping_sunday/ShoppingSundayFragment.kt
@@ -4,33 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.cherit.perpetualcalendar.R
+import com.cherit.perpetualcalendar.ui.components.event_list.EventListRecyclerViewAdapter
 import com.cherit.perpetualcalendar.ui.views.year_selection.YearSelectionViewModel
+import com.cherit.perpetualcalendar.utils.getShoppingSundayList
 
 class ShoppingSundayFragment : Fragment() {
 
     private lateinit var shoppingSundayViewModel: ShoppingSundayViewModel
-    private lateinit var yearSelectedViewModel: YearSelectionViewModel
+    private lateinit var yearSelectionViewModel: YearSelectionViewModel
+    private lateinit var shoppingSundayList: RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
         shoppingSundayViewModel =
-                ViewModelProvider(this).get(ShoppingSundayViewModel::class.java)
-        yearSelectedViewModel =
-            ViewModelProvider(this).get(YearSelectionViewModel::class.java)
+            ViewModelProvider(this).get(ShoppingSundayViewModel::class.java)
+
+        yearSelectionViewModel =
+            ViewModelProvider(requireActivity()).get(YearSelectionViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_shopping_sunday, container, false)
-
-        shoppingSundayViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        shoppingSundayList = root.findViewById(R.id.shopping_sunday_list)
+        setViewModelListener()
+        shoppingSundayViewModel.shoppingSundays.value = getShoppingSundayList(yearSelectionViewModel.selectedYear.value!!)
         return root
+    }
+
+    private fun setViewModelListener() {
+        shoppingSundayViewModel.shoppingSundays.observe(viewLifecycleOwner, {
+            shoppingSundayList.adapter = EventListRecyclerViewAdapter(it)
+        })
     }
 }
--- app/src/main/java/com/cherit/perpetualcalendar/ui/views/shopping_sunday/ShoppingSundayViewModel.kt
+++ app/src/main/java/com/cherit/perpetualcalendar/ui/views/shopping_sunday/ShoppingSundayViewModel.kt
@@ -3,11 +3,13 @@ package com.cherit.perpetualcalendar.ui.views.shopping_sunday
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cherit.perpetualcalendar.models.Event
+import com.cherit.perpetualcalendar.utils.getShoppingSundayList
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ShoppingSundayViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
+    var shoppingSundays: MutableLiveData<ArrayList<Event>> = MutableLiveData<ArrayList<Event>>()
+
 }
